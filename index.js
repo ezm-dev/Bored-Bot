7dc7f26f6a652af5d3022776bbae50e3ca737bbe
ezm-dev <ezm.sabry@gmail.com>
2025-12-11 14:43:07 +1000

    Modify the response style
    Add a class to modify the style of the idea after generated on the screen.

--- index.js
+++ index.js
@@ -3,5 +3,10 @@ const idea = document.getElementById("idea")
 document.getElementById("btn").addEventListener("click", ()=>{
     fetch("https://apis.scrimba.com/bored/api/activity")
     .then(data=>data.json())
-    .then(ideaText=>idea.textContent = ideaText.activity)
+    .then(ideaText=>{
+        idea.textContent = ideaText.activity
+        idea.classList.add("ideaText")
+
+    })
+    
 })
