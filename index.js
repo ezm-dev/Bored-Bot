ec09f549535a253720db1531baa668d1b3479783
ezm-dev <ezm.sabry@gmail.com>
2025-12-11 13:50:34 +1000

    Add button functionality
    Add button functionality to fetch idea from API and show it on the screen

---
+++ index.js
@@ -0,0 +1,7 @@
+
+const idea = document.getElementById("idea")
+document.getElementById("btn").addEventListener("click", ()=>{
+    fetch(" https://apis.scrimba.com/bored/api/activity")
+    .then(data=>data.json())
+    .then(ideaText=>idea.textContent = ideaText.activity)
+})
